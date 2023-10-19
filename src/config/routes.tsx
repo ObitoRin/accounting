184bfc0c7a8a4e66c0315a41393dabace17da3e9
ObitoRin <2209246794@qq.com>
2023-10-19 16:41:47 +0800

    feature：创建欢迎页面

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,7 +1,23 @@
+import { RouteRecordRaw } from "vue-router";
 import { Bar } from "../views/Bar";
 import { Foo } from "../views/Foo";
+import { Welcome } from "../views/Welcome";
+import { First } from "../components/welcome/first";
+import { Second } from "../components/welcome/second";
+import { Third } from "../components/welcome/third";
+import { Fourth } from "../components/welcome/fourth";
 
-export const routes = [
+export const routes: RouteRecordRaw[] = [
   { path: '/', component: Foo },
   { path: '/about', component: Bar },
+  {
+    path: '/welcome',
+    component: Welcome,
+    children: [
+      { path: 'first', component: First },
+      { path: 'second', component: Second },
+      { path: 'third', component: Third },
+      { path: 'fourth', component: Fourth },
+    ]
+  }
 ]
