f1cbad8cc63955cdd425e1bb8675cda193b69be0
ObitoRin <2209246794@qq.com>
2023-10-24 14:06:34 +0800

    修改默认路由

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,6 +1,4 @@
 import { RouteRecordRaw } from "vue-router";
-import { Bar } from "../views/Bar";
-import { Foo } from "../views/Foo";
 import { Welcome } from "../views/Welcome";
 import { First } from "../components/welcome/First";
 import { Second } from "../components/welcome/Second";
@@ -8,16 +6,16 @@ import { Third } from "../components/welcome/Third";
 import { Fourth } from "../components/welcome/Fourth";
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/', component: Foo },
-  { path: '/about', component: Bar },
+  { path: '/', redirect: '/welcome' },
   {
     path: '/welcome',
     component: Welcome,
     children: [
-      { path: 'first', component: First },
-      { path: 'second', component: Second },
-      { path: 'third', component: Third },
-      { path: 'fourth', component: Fourth },
+      { path: '', redirect: '/welcome/1' },
+      { path: '1', component: First },
+      { path: '2', component: Second },
+      { path: '3', component: Third },
+      { path: '4', component: Fourth },
     ]
   }
 ]
