a910d3f3eaea90e7af957d9b296075af9e2eb017
ObitoRin <2209246794@qq.com>
2023-10-30 10:28:08 +0800

    添加 MainLayout 布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,9 +4,9 @@ import s from './StartPage.module.scss'
 import { FloatButton } from '../shared/FloatButton';
 import { Center } from '../shared/Center';
 import { Icon } from '../shared/Icon';
-import { Navbar } from '../shared/Navbar';
 import { Overlay } from '../shared/Overlay';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from './layouts/MainLayout';
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -16,28 +16,28 @@ export const StartPage = defineComponent({
     }
 
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => '记账',
-            icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu} />
-          }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton name='add' />
-        </RouterLink>
-        {refOverlayVisible.value &&
-          <Overlay onClose={() => refOverlayVisible.value = false} />
-        }
-      </div>
+      <MainLayout>
+        {{
+          title: () => '记账',
+          icon: () => <Icon name='menu' class={s.navIcon} onClick={onClickMenu} />,
+          default: () => <>
+            <Center class={s.pig_wrapper}>
+              <Icon name="pig" class={s.pig} />
+            </Center>
+            <div class={s.button_wrapper}>
+              <RouterLink to="/items/create">
+                <Button class={s.button}>开始记账</Button>
+              </RouterLink>
+            </div>
+            <RouterLink to="/items/create">
+              <FloatButton name='add' />
+            </RouterLink>
+            {refOverlayVisible.value &&
+              <Overlay onClose={() => refOverlayVisible.value = false} />
+            }
+          </>
+        }}
+      </MainLayout>
     )
   }
 })
---
+++ src/views/layouts/MainLayout.tsx
@@ -0,0 +1,19 @@
+import { defineComponent } from 'vue';
+import { Navbar } from '../../shared/Navbar';
+
+export const MainLayout = defineComponent({
+  setup: (props, context) => {
+    const { slots } = context
+    return () => (
+      <div>
+        <Navbar>
+          {{
+            default: () => slots.title?.(),
+            icon: () => slots.icon?.()
+          }}
+        </Navbar>
+        {slots.default?.()}
+      </div>
+    )
+  }
+})
