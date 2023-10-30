90f2548fea1433f11505abb6931f6bd1353b344a
ObitoRin <2209246794@qq.com>
2023-10-30 16:15:26 +0800

    wip: Grid 布局数字按键样式

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -14,23 +14,25 @@ export const InputPad = defineComponent({
     const now = new Date()
     const refDate = ref<Date>(now)
     const refDatePickerVisible = ref(false)
+    const refAmount = ref('')
+
+    const appendText = (n: string | number) => {
+      refAmount.value += n.toString()
+    }
 
     const buttons = [
-      { text: '1', onClick: () => { } },
-      { text: '2', onClick: () => { } },
-      { text: '3', onClick: () => { } },
-      { text: '清空', onClick: () => { } },
-      { text: '4', onClick: () => { } },
-      { text: '5', onClick: () => { } },
-      { text: '6', onClick: () => { } },
-      { text: '+', onClick: () => { } },
-      { text: '7', onClick: () => { } },
-      { text: '8', onClick: () => { } },
-      { text: '9', onClick: () => { } },
-      { text: '-', onClick: () => { } },
-      { text: '.', onClick: () => { } },
-      { text: '0', onClick: () => { } },
-      { text: '删', onClick: () => { } },
+      { text: '1', onClick: () => { appendText(1) } },
+      { text: '2', onClick: () => { appendText(2) } },
+      { text: '3', onClick: () => { appendText(3) } },
+      { text: '4', onClick: () => { appendText(4) } },
+      { text: '5', onClick: () => { appendText(5) } },
+      { text: '6', onClick: () => { appendText(6) } },
+      { text: '7', onClick: () => { appendText(7) } },
+      { text: '8', onClick: () => { appendText(8) } },
+      { text: '9', onClick: () => { appendText(9) } },
+      { text: '.', onClick: () => { appendText('.') } },
+      { text: '0', onClick: () => { appendText(0) } },
+      { text: '清空', onClick: () => { refAmount.value = '' } },
       { text: '提交', onClick: () => { } },
     ]
 
@@ -56,7 +58,7 @@ export const InputPad = defineComponent({
             />
           </Popup>
         </span>
-        <span class={s.amount}>199.66</span>
+        <span class={s.amount}>{refAmount.value}</span>
       </div>
       <div class={s.buttons}>
         {buttons.map(item => <button onClick={item.onClick}>{item.text}</button>)}
