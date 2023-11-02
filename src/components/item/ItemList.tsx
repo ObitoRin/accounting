170078f107fe7391bba4f087b48477eda1d20830
ObitoRin <2209246794@qq.com>
2023-11-02 15:18:54 +0800

    Tabs 组件添加 classPrefix

--- src/components/item/ItemList.tsx
+++ src/components/item/ItemList.tsx
@@ -1,14 +1,40 @@
-import { defineComponent, PropType } from 'vue';
+import { defineComponent, PropType, ref } from 'vue';
 import s from './ItemList.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
+import { Tab, Tabs } from '../../shared/Tabs';
+
 export const ItemList = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
-  setup: (props, context) => {
-    return () => (
-      <div class={s.wrapper}></div>
-    )
-  }
+  props: {
+    name: {
+      type: String as PropType<string>
+    }
+  },
+  setup: (props, context) => {
+    const refSelected = ref('本月')
+    return () => (
+      <MainLayout>
+        {{
+          title: () => '记账',
+          icon: () => <Icon name='menu'/>,
+          default: () => (
+            <Tabs v-model:selected={refSelected.value} classPrefix='customTabs'>
+              <Tab name="本月">
+                list 1
+              </Tab>
+              <Tab name="上月">
+                list 2
+              </Tab>
+              <Tab name="今年">
+                list 3
+              </Tab>
+              <Tab name="自定义时间">
+                list 4
+              </Tab>
+            </Tabs>
+          )
+        }}
+      </MainLayout>
+    )
+  }
 })
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -8,6 +8,9 @@ export const Tabs = defineComponent({
     },
     onUpdateSelected: {
       type: Function as PropType<(name: string) => void>
+    },
+    classPrefix: {
+      type: String
     }
   },
   setup: (props, context) => {
@@ -18,18 +21,19 @@ export const Tabs = defineComponent({
         throw new Error('<Tabs> only accepts <Tab> as children')
       }
     }
+    const cp = props.classPrefix;
 
     return () => (
-      <div class={s.tabs}>
-        <ol class={s.tabs_nav}>
+      <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
           {tabs.map(item =>
-            <li class={item.props?.name === props.selected ? s.selected : ''}
+            <li class={[item.props?.name === props.selected ? [s.selected, cp + '_selected'] : '', cp + '_tabs_nav_item']}
               onClick={() => context.emit('update:selected', item.props?.name)}
             // onClick={() => props.onUpdateSelected?.(item.props?.name)}
             >{item.props?.name}</li>
           )}
         </ol>
-        <div>{tabs.find(item=> item.props?.name === props.selected)}</div>
+        <div>{tabs.find(item => item.props?.name === props.selected)}</div>
       </div>
     )
   }
