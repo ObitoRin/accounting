ae7f34afe022bdf40eeef467f4c672e0a060a097
ObitoRin <2209246794@qq.com>
2024-01-20 18:00:31 +0800

    优化登录校验

--- src/main.ts
+++ src/main.ts
@@ -9,17 +9,28 @@ import { promiseMe, fetchMe } from './shared/Me';
 const router = createRouter({ history, routes })
 
 fetchMe()
-router.beforeEach(async (to, from) => {
-  if (to.path === '/' || to.path === '/start' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in')) {
-    return true
-  } else {
-    const path = await promiseMe!
-      .then(
-        () => true,
-        () => '/sign_in?return_to=' + to.fullPath
-      )
-    return path
+
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+  '/': 'exact',
+  '/start': 'exact',
+  '/welcome': 'startsWith',
+  '/sign_in': 'startsWith',
+}
+router.beforeEach((to, from) => {
+  for (const key in whiteList) {
+    const value = whiteList[key]
+    if (value === 'exact' && key === to.path) {
+      return true
+    }
+    if (value === 'startsWith' && to.path.startsWith(key)) {
+      return true
+    }
   }
+  return promiseMe!
+    .then(
+      () => true,
+      () => '/sign_in?return_to=' + to.path
+    )
 })
 
 const app = createApp(App)
