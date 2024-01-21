2c92cd005430e518e1460ae0838ffc44933218af
ObitoRin <2209246794@qq.com>
2024-01-21 15:48:52 +0800

    mock tags

--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -42,9 +42,8 @@ export const SignInPage = defineComponent({
       ]))
 
       if (!hasError(errors)) {
-        const response = await http.post<{ jwt: string }>('/session', formData, {
-          params: { _mock: 'session' }
-        }).catch(onError)
+        const response = await http.post<{ jwt: string }>('/session', formData)
+          .catch(onError)
         localStorage.setItem('jwt', response.data.jwt)
 
         refreshMe()
