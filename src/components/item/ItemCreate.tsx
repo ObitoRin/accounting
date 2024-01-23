11b0477426da69435d5e9fe39e669bfadae168cb
ObitoRin <2209246794@qq.com>
2024-01-23 10:56:47 +0800

    封装 useTags

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -6,6 +6,7 @@ import { Tabs, Tab } from '../../shared/Tabs';
 import { InputPad } from './InputPad';
 import { http } from '../../shared/Http';
 import { Button } from '../../shared/Button';
+import { useTags } from '../../hooks/useTags';
 
 export const ItemCreate = defineComponent({
   props: {
@@ -18,28 +19,19 @@ export const ItemCreate = defineComponent({
     //   refKind.value = name
     // }
     const refKind = ref('支出')
-    const refExpensesTags = ref<Tag[]>([])
-    const refIncomeTags = ref<Tag[]>([])
-    const refPage = ref(0)
-    const refHasMore = ref(false)
-    const fetchTags = async () => {
-      const response = await http.get<Resources<Tag>>('/tags', { 
+    const { tags: expensesTag, hasMore, fetchTags } = useTags((page) => {
+      return http.get<Resources<Tag>>('/tags', { 
         kind: 'expenses',
+        page: page + 1,
         _mock: 'tagIndex',
-        page: refPage.value + 1
       })
-      const { resources, pager } = response.data
-      refExpensesTags.value.push(...resources)
-      refHasMore.value = (pager.page - 1) * pager.per_page + resources.length < pager.count
-      refPage.value += 1
-    }
-    onMounted(fetchTags)
-    onMounted(async () => {
-      const response = await http.get<{ resource: Tag[] }>('/tags', { 
+    })
+    const { tags: incomeTags, hasMore: hasMoreIncome, fetchTags: fetchTagsIncome } = useTags((page) => {
+      return http.get<Resources<Tag>>('/tags', {
         kind: 'income',
+        page: page + 1,
         _mock: 'tagIndex'
       })
-      refIncomeTags.value = response.data.resource
     })
     return () => (
       <div class={s.wrapper}>
@@ -61,7 +53,7 @@ export const ItemCreate = defineComponent({
                       新增
                     </div>
                   </div>
-                  {refExpensesTags.value.map(tag =>
+                  {expensesTag.value.map(tag =>
                     <div class={[s.tag, s.selected]}>
                       <div class={s.sign}>
                         {tag.sign}
@@ -73,31 +65,39 @@ export const ItemCreate = defineComponent({
                   )}
                 </div>
                 <div class={s.more}>
-                  {refHasMore ? 
+                  {hasMore ? 
                     <Button onClick={fetchTags}>加载更多</Button> :
                     <span>没有更多</span>
                   }
                 </div>
               </Tab>
-              <Tab name="收入" class={s.tags_wrapper}>
-                <div class={s.tag}>
-                  <div class={s.sign}>
-                    <Icon name="add" class={s.createTag} />
-                  </div>
-                  <div class={s.name}>
-                    新增
-                  </div>
-                </div>
-                {refIncomeTags.value.map(tag =>
-                  <div class={[s.tag, s.selected]}>
+              <Tab name="收入">
+                <div class={s.tags_wrapper}>
+                  <div class={s.tag}>
                     <div class={s.sign}>
-                      {tag.sign}
+                      <Icon name="add" class={s.createTag} />
                     </div>
                     <div class={s.name}>
-                      {tag.name}
+                      新增
                     </div>
                   </div>
-                )}
+                  {incomeTags.value.map(tag =>
+                    <div class={[s.tag, s.selected]}>
+                      <div class={s.sign}>
+                        {tag.sign}
+                      </div>
+                      <div class={s.name}>
+                        {tag.name}
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <div class={s.more}>
+                  {hasMoreIncome ? 
+                    <Button onClick={fetchTagsIncome}>加载更多</Button> :
+                    <span>没有更多</span>
+                  }
+                </div>
               </Tab>
             </Tabs>
             <div class={s.inputPad_wrapper}>
