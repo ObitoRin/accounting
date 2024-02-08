06b1fce724e3584044373c7cccd26f395f8c5f97
ObitoRin <2209246794@qq.com>
2024-02-08 16:57:27 +0800

    饼图数据

--- src/components/statistics/Chart.tsx
+++ src/components/statistics/Chart.tsx
@@ -11,6 +11,8 @@ const DAY = 24 * 3600 * 1000
 
 type Data1Item = { happen_at: string; amount: number }
 type Data1 = Data1Item[]
+type Data2Item = { tag_id: number; tag: Tag; amount: number }
+type Data2 = Data2Item[]
 
 export const Chart = defineComponent({
   props: {
@@ -48,11 +50,29 @@ export const Chart = defineComponent({
         happen_after: props.startDate,
         happen_before: props.endDate,
         kind: kind.value,
+        group_by: 'happen_at',
         _mock: 'itemSummary'
       })
       data1.value = response.data.groups
     })
 
+    const data2 = ref<Data2>([])
+    const betterData2 = computed<{ name: string; value: number }[]>(() =>
+      data2.value.map(item => ({
+        name: item.tag.name,
+        value: item.amount,
+      }))
+    )
+    onMounted(async () => {
+      const response = await http.get<{ groups: Data2, summary: number }>('/items/summary', {
+        happen_after: props.startDate,
+        happen_before: props.endDate,
+        kind: kind.value,
+        group_by: 'tag_id',
+        _mock: 'itemSummary'
+      })
+      data2.value = response.data.groups
+    })
     return () => (
       <div class={s.wrapper}>
         <FormItem
@@ -65,7 +85,7 @@ export const Chart = defineComponent({
           ]}
         />
         <LineChart data={betterData1.value} />
-        <PieChart />
+        <PieChart data={betterData2.value} />
         <Bars />
       </div>
     )
--- src/components/statistics/PieChart.tsx
+++ src/components/statistics/PieChart.tsx
@@ -1,45 +1,63 @@
-import { defineComponent, onMounted, PropType, ref } from 'vue';
+import { defineComponent, onMounted, PropType, ref, watch } from 'vue';
 import s from './PieChart.module.scss';
 import * as echarts from 'echarts';
+import { getMoney } from '../../shared/Money';
+
+const echartsOption = {
+  tooltip: {
+    trigger: 'item',
+    formatter: (x: { name: string, value: number, percent: number }) => {
+      const { name, value, percent } = x
+      return `${name}: ￥${getMoney(value)} 占比 ${percent}%`
+    }
+  },
+  grid: [
+    { left: 0, top: 0, right: 0, bottom: 0 }
+  ],
+  series: [
+    {
+      type: 'pie',
+      radius: '60%',
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+}
 
 export const PieChart = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
-  setup: (props, context) => {
-  const refDiv2 = ref<HTMLDivElement>()
-  onMounted(() => {
-    if (!refDiv2.value) { return }
-    const myChart = echarts.init(refDiv2.value);
-    const option = {
-      series: [
-        {
-          name: 'Access From',
-          type: 'pie',
-          radius: '50%',
-          data: [
-            { value: 1048, name: 'Search Engine' },
-            { value: 735, name: 'Direct' },
-            { value: 580, name: 'Email' },
-            { value: 484, name: 'Union Ads' },
-            { value: 300, name: 'Video Ads' }
-          ],
-          emphasis: {
-            itemStyle: {
-              shadowBlur: 10,
-              shadowOffsetX: 0,
-              shadowColor: 'rgba(0, 0, 0, 0.5)'
-            }
-          }
-        }
-      ]
+  props: {
+    name: {
+      type: String as PropType<string>
+    },
+    data: {
+      type: Array as PropType<{ name: string; value: number }[]>
     }
-    myChart.setOption(option)
-  })
-    return () => (
-      <div ref={refDiv2} class={s.wrapper}></div>
-    )
-  }
+  },
+  setup: (props, context) => {
+    const refDiv2 = ref<HTMLDivElement>()
+    let chart: echarts.ECharts | undefined = undefined
+    onMounted(() => {
+      if (!refDiv2.value) { return }
+      chart = echarts.init(refDiv2.value);
+      const option = echartsOption
+      chart.setOption(option)
+    })
+    watch(() => props.data, () => {
+      chart?.setOption({
+        series: [
+          {
+            data: props.data,
+          }
+        ]
+      })
+    })
+    return () => (
+      <div ref={refDiv2} class={s.wrapper}></div>
+    )
+  }
 })
