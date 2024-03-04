17875898e06712b53c230e789b08fbc8476899d5
ObitoRin <2209246794@qq.com>
2024-03-04 16:56:56 +0800

    生产不打包 mock

--- vite.config.ts
+++ vite.config.ts
@@ -6,43 +6,51 @@ import styleImport, { VantResolve } from 'vite-plugin-style-import';
 import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id: any) {
-          if (id.includes('echarts')) {
-            return 'echarts';
-          }
-          if (id.includes('mock') || id.includes('faker')) {
-            return 'mock';
-          }
-          if (id.includes('vant')) {
-            return 'vant';
-          }
-          if (id.includes('node_modules')) {
-            return 'vendor';
+export default defineConfig(({ command }) => {
+  return {
+    // base: './',
+    define: command === 'build' ? {
+      DEBUG: false
+    } : {
+      DEBUG: true
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes('echarts')) {
+              return 'echarts';
+            }
+            if (id.includes('mock') || id.includes('faker')) {
+              return 'mock';
+            }
+            if (id.includes('vant')) {
+              return 'vant';
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
           }
         }
       }
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx({
-      transformOn: true,
-      mergeProps: true      
-    }),
-    svgstore(),
-    styleImport({
-      resolves: [VantResolve()],
-    }),
-  ],
-  server: {
-    host: true,
-    proxy: {
-      '/api/v1': 'http://121.196.236.94:3000/'
+    },
+    plugins: [
+      vue(),
+      vueJsx({
+        transformOn: true,
+        mergeProps: true
+      }),
+      svgstore(),
+      styleImport({
+        resolves: [VantResolve()],
+      }),
+    ],
+    server: {
+      host: true,
+      proxy: {
+        '/api/v1': 'http://121.196.236.94:3000/'
+      },
+      open: true
     }
   }
 })
