a1c8763a0e95be2b4281b61070620e8809f57145
ObitoRin <2209246794@qq.com>
2024-03-05 21:01:54 +0800

    支出收入金额颜色

--- src/components/item/ItemSummary.tsx
+++ src/components/item/ItemSummary.tsx
@@ -70,7 +70,7 @@ export const ItemSummary = defineComponent({
                 <div class={s.text}>
                   <div class={s.tagAndAmount}>
                     <span class={s.tag}>{item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
-                    <span class={s.amount}><Money value={item.amount} /></span>
+                    <span style={{ color: item.kind === 'expenses' ? '#53A867' : item.kind === 'income' ? '#FE7275' : 'unset' }}><Money value={item.amount} /></span>
                   </div>
                   <div class={s.time}>
                     <Datetime value={item.happen_at} />
